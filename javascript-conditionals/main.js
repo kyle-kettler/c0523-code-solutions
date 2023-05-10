698d90d9457f16e7d1383253b319b6a531153644
Kyle Kettler <hello@kylekettler.com>
2023-05-10 20:46:19 +0000

    completed javascript-conditionals

---
+++ javascript-conditionals/main.js
@@ -0,0 +1,106 @@
+/* exported isUnderFive,
+            isEven,
+            startsWithJ,
+            isOldEnoughToDrink,
+            isOldEnoughToDrive,
+            isOldEnoughToDrinkAndDrive,
+            categorizeAcidity,
+            introduceWarnerBro,
+            recommendMovie
+ */
+function isUnderFive(number) {
+  if (number < 5) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function isEven(number) {
+  if (number % 2 === 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function startsWithJ(string) {
+  if (string[0] === 'J') {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function isOldEnoughToDrink(person) {
+  if (person.age >= 21) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function isOldEnoughToDrive(person) {
+  if (person.age >= 16) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function isOldEnoughToDrinkAndDrive(person) {
+  return false;
+}
+
+function categorizeAcidity(pH) {
+  if (pH > 0 && pH < 7) {
+    return 'acid';
+  } else if (pH === 7) {
+    return 'neutral';
+  } else if (pH > 7 && pH < 14) {
+    return 'base';
+  } else {
+    return 'invalid pH level';
+  }
+}
+
+function introduceWarnerBro(name) {
+  switch (name) {
+    case 'yakko':
+    case 'wakko':
+      console.log("We're the warner brothers!");
+      break;
+    case 'dot':
+      console.log("I'm cute~");
+      break;
+    default:
+      console.log('Goodnight everybody!');
+  }
+}
+
+function recommendMovie(genre) {
+  switch (genre) {
+    case 'action':
+      console.log('John Wick');
+      break;
+    case 'comedy':
+      console.log('The Big Lebowski');
+      break;
+    case 'horror':
+      console.log('The Wytch');
+      break;
+    case 'drama':
+      console.log('The Lighthouse');
+      break;
+    case 'musical':
+      console.log('La La Land');
+      break;
+    case 'sci-fi':
+      console.log('Dune');
+      break;
+    default:
+      console.log(
+        'Genre not recognized. Choose between action, comedy, horror, drama, musical, or sci-fi'
+      );
+  }
+}
