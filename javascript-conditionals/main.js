89032a16570a18a9f0cc2bc61676a04ede9e7a73
Kyle Kettler <hello@kylekettler.com>
2023-05-10 20:56:43 +0000

    fixed issue on categorizeAcidity function

--- javascript-conditionals/main.js
+++ javascript-conditionals/main.js
@@ -53,11 +53,11 @@ function isOldEnoughToDrinkAndDrive(person) {
 }
 
 function categorizeAcidity(pH) {
-  if (pH > 0 && pH < 7) {
+  if (pH >= 0 && pH < 7) {
     return 'acid';
   } else if (pH === 7) {
     return 'neutral';
-  } else if (pH > 7 && pH < 14) {
+  } else if (pH > 7 && pH <= 14) {
     return 'base';
   } else {
     return 'invalid pH level';
