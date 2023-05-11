e184b1b26e4800623289f5867e6a644a47aeac93
Kyle Kettler <hello@kylekettler.com>
2023-05-11 19:02:24 +0000

    removed console.log

--- javascript-loops-practice/filter-out-nulls.js
+++ javascript-loops-practice/filter-out-nulls.js
@@ -2,7 +2,6 @@
 function filterOutNulls(values) {
   const filteredValues = [];
   for (let i = 0; i < values.length; i++) {
-    console.log(values[i]);
     if (values[i] !== null) {
       filteredValues.push(values[i]);
     }
--- javascript-loops-practice/sum-all.js
+++ javascript-loops-practice/sum-all.js
@@ -3,7 +3,6 @@ function sumAll(numbers) {
   let sum = 0;
   for (let i = 0; i < numbers.length; i++) {
     sum += numbers[i];
-    console.log(numbers[i]);
   }
   return sum;
 }
