511e788cefbf5babd637252cf1abecc35a8fa55e
Kyle Kettler <hello@kylekettler.com>
2023-06-06 23:02:41 +0000

    completed javascript-ajax

---
+++ javascript-ajax/users.js
@@ -0,0 +1,14 @@
+const $userList = document.querySelector('#user-list');
+const xhr = new XMLHttpRequest();
+xhr.open('GET', 'https://jsonplaceholder.typicode.com/users');
+xhr.responseType = 'json';
+xhr.addEventListener('load', () => {
+  console.log(xhr.status);
+  console.log(xhr.response);
+  for (let i = 0; i < xhr.response.length; i++) {
+    const $li = document.createElement('li');
+    $li.textContent = xhr.response[i].name;
+    $userList.appendChild($li);
+  }
+});
+xhr.send();
