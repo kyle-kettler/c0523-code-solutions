0135934a9531cf7c11fe2e94527a37e66fc1dfd8
Kyle Kettler <hello@kylekettler.com>
2023-06-06 15:53:27 +0000

    carousel autoplay and looping working

--- javascript-carousel/main.js
+++ javascript-carousel/main.js
@@ -5,15 +5,17 @@ const $carousel = document.querySelector('.carousel-wrap');
 const $prevArrow = document.querySelector('#prev');
 const $nextArrow = document.querySelector('#next');
 
+const totalItemsIndex = $imagesList.length - 1;
+
 let counter = 0;
 
 function nextSlide() {
-  if (counter === $imagesList.length - 1) {
+  if (counter === totalItemsIndex) {
     counter = 0;
     $paginationDots[0].classList.add('active');
     $imagesList[0].classList.remove('hidden');
-    $paginationDots[$imagesList.length - 1].classList.remove('active');
-    $imagesList[$imagesList.length - 1].classList.add('hidden');
+    $paginationDots[totalItemsIndex].classList.remove('active');
+    $imagesList[totalItemsIndex].classList.add('hidden');
   } else {
     $paginationDots[counter].classList.remove('active');
     $paginationDots[counter + 1].classList.add('active');
@@ -24,19 +26,33 @@ function nextSlide() {
 }
 
 function prevSlide() {
-  $paginationDots[counter].classList.remove('active');
-  $paginationDots[counter - 1].classList.add('active');
-  $imagesList[counter].classList.add('hidden');
-  $imagesList[counter - 1].classList.remove('hidden');
-  counter--;
+  if (counter === 0) {
+    counter = totalItemsIndex;
+    $paginationDots[totalItemsIndex].classList.add('active');
+    $imagesList[totalItemsIndex].classList.remove('hidden');
+    $paginationDots[0].classList.remove('active');
+    $imagesList[0].classList.add('hidden');
+  } else {
+    $paginationDots[counter].classList.remove('active');
+    $paginationDots[counter - 1].classList.add('active');
+    $imagesList[counter].classList.add('hidden');
+    $imagesList[counter - 1].classList.remove('hidden');
+    counter--;
+  }
 }
 
 $carousel.addEventListener('click', (event) => {
   if (event.target === $nextArrow) {
     nextSlide();
-  } else if (event.target === $prevArrow && counter > 0) {
+    clearInterval(carouselTimer);
+    setInterval(nextSlide, 3000);
+  } else if (event.target === $prevArrow) {
     prevSlide();
+    clearInterval(carouselTimer);
+    setInterval(nextSlide, 3000);
   } else if (event.target === $paginationDots) {
     console.log($paginationDots.indexOf());
   }
 });
+
+const carouselTimer = setInterval(nextSlide, 3000);
