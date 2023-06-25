4c5da2b5afab3b0e5f7e8d0397c458b36f00ad8d
Kyle Kettler <hello@kylekettler.com>
2023-06-25 19:21:28 +0000

    WIP carousel working, just need to figure out how to reset the timer properly when the next or prev arrow is clicked.

--- javascript-carousel/main.js
+++ javascript-carousel/main.js
@@ -1,59 +1,103 @@
-const $imagesList = document.querySelectorAll('.carousel-img');
-const $paginationDots = document.querySelectorAll('.pagination-dot');
-// const $pagination = document.querySelector('.pagination');
-const $carousel = document.querySelector('.carousel-wrap');
+const $carouselTrack = document.querySelector('.carousel-track');
+const $slideList = Array.from($carouselTrack.children);
 const $prevArrow = document.querySelector('#prev');
 const $nextArrow = document.querySelector('#next');
-
-const totalItemsIndex = $imagesList.length - 1;
-
-let counter = 0;
+const $pagination = document.querySelector('.pagination');
+const $paginationDots = Array.from($pagination.children);
 
 function nextSlide() {
-  if (counter === totalItemsIndex) {
-    counter = 0;
+  const $currentSlide = $carouselTrack.querySelector('.current');
+  const $nextSlide = $currentSlide.nextElementSibling;
+  const $currentDot = $pagination.querySelector('.active');
+  const $nextDot = $currentDot.nextElementSibling;
+  const currentIndex = $slideList.findIndex(
+    ($slideList) => $slideList === $currentSlide
+  );
+
+  if (currentIndex === $slideList.length - 1) {
+    $currentSlide.classList.remove('current');
+    $currentDot.classList.remove('active');
+    $slideList[0].classList.add('current');
     $paginationDots[0].classList.add('active');
-    $imagesList[0].classList.remove('hidden');
-    $paginationDots[totalItemsIndex].classList.remove('active');
-    $imagesList[totalItemsIndex].classList.add('hidden');
   } else {
-    $paginationDots[counter].classList.remove('active');
-    $paginationDots[counter + 1].classList.add('active');
-    $imagesList[counter].classList.add('hidden');
-    $imagesList[counter + 1].classList.remove('hidden');
-    counter++;
+    $currentSlide.classList.remove('current');
+    $nextSlide.classList.add('current');
+    $currentDot.classList.remove('active');
+    $nextDot.classList.add('active');
   }
 }
 
 function prevSlide() {
-  if (counter === 0) {
-    counter = totalItemsIndex;
-    $paginationDots[totalItemsIndex].classList.add('active');
-    $imagesList[totalItemsIndex].classList.remove('hidden');
-    $paginationDots[0].classList.remove('active');
-    $imagesList[0].classList.add('hidden');
+  const $currentSlide = $carouselTrack.querySelector('.current');
+  const $previousSlide = $currentSlide.previousElementSibling;
+  const $currentDot = $pagination.querySelector('.active');
+  const $prevDot = $currentDot.previousElementSibling;
+  const currentIndex = $slideList.findIndex(
+    ($slideList) => $slideList === $currentSlide
+  );
+
+  if (currentIndex === 0) {
+    $currentSlide.classList.remove('current');
+    $currentDot.classList.remove('active');
+    $slideList[$slideList.length - 1].classList.add('current');
+    $paginationDots[$paginationDots.length - 1].classList.add('active');
   } else {
-    $paginationDots[counter].classList.remove('active');
-    $paginationDots[counter - 1].classList.add('active');
-    $imagesList[counter].classList.add('hidden');
-    $imagesList[counter - 1].classList.remove('hidden');
-    counter--;
+    $previousSlide.classList.add('current');
+    $currentSlide.classList.remove('current');
+    $currentDot.classList.remove('active');
+    $prevDot.classList.add('active');
   }
+  //   if (counter === 0) {
+  //     counter = totalItemsIndex;
+  //     $paginationDots[totalItemsIndex].classList.add('active');
+  //     $imagesList[totalItemsIndex].classList.remove('hidden');
+  //     $paginationDots[0].classList.remove('active');
+  //     $imagesList[0].classList.add('hidden');
+  //   } else {
+  //     $paginationDots[counter].classList.remove('active');
+  //     $paginationDots[counter - 1].classList.add('active');
+  //     $imagesList[counter].classList.add('hidden');
+  //     $imagesList[counter - 1].classList.remove('hidden');
+  //     counter--;
+  //   }
 }
 
-$carousel.addEventListener('click', (event) => {
-  console.log(event.targe);
-  if (event.target === $nextArrow) {
-    nextSlide();
-    clearInterval(carouselTimer);
-    setInterval(nextSlide, 3000);
-  } else if (event.target === $prevArrow) {
-    prevSlide();
-    clearInterval(carouselTimer);
-    setInterval(nextSlide, 3000);
-  } else if (event.target === $paginationDots) {
-    console.log(event.target);
-  }
-});
+function goToSlideAtIndex(event) {
+  const $targetDot = event.target.closest('button');
+  if (!$targetDot) return;
 
-const carouselTimer = setInterval(nextSlide, 3000);
+  const $currentSlide = $carouselTrack.querySelector('.current');
+  const $currentDot = $pagination.querySelector('.active');
+  const targetIndex = $paginationDots.findIndex(
+    ($paginationDots) => $paginationDots === $targetDot
+  );
+  const $targetSlide = $slideList[targetIndex];
+
+  $currentSlide.classList.remove('current');
+  $currentDot.classList.remove('active');
+  $targetDot.classList.add('active');
+  $targetSlide.classList.add('current');
+}
+
+$pagination.addEventListener('click', goToSlideAtIndex);
+
+$nextArrow.addEventListener('click', nextSlide);
+
+$prevArrow.addEventListener('click', prevSlide);
+
+// $carousel.addEventListener('click', (event) => {
+//   console.log(event.target);
+//   if (event.target === $nextArrow) {
+//     nextSlide();
+//     clearInterval(carouselTimer);
+//     setInterval(nextSlide, 3000);
+//   } else if (event.target === $prevArrow) {
+//     prevSlide();
+//     clearInterval(carouselTimer);
+//     setInterval(nextSlide, 3000);
+//   } else if (event.target === $paginationDots) {
+//     console.log(event.target);
+//   }
+// });
+
+// const carouselTimer = setInterval(nextSlide, 3000);
