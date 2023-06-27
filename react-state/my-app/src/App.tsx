332a18cef044d3a8f61b68baaa86c4e9d7d0ff41
Kyle Kettler <hello@kylekettler.com>
2023-06-27 16:37:21 +0000

    reset files

--- react-state/my-app/src/App.tsx
+++ react-state/my-app/src/App.tsx
@@ -1,13 +1,23 @@
 import React from 'react';
+import logo from './logo.svg';
 import './App.css';
-import ToggleButton from './ToggleButton';
 
 function App() {
   return (
-    <div>
-      <ToggleButton text={'Hello'} color={'red'} />
-      <ToggleButton text={'World'} color={'green'} />
-      <ToggleButton text={'React'} color={'cornflowerblue'} />
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer">
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
