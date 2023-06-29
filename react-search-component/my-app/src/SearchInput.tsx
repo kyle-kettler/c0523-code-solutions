f1cc1bdad26f3e837ef42a2efcadcae8fec7ac2d
Kyle Kettler <hello@kylekettler.com>
2023-06-29 23:46:15 +0000

    updated search input so it has a magnifying glass icon

--- react-search-component/my-app/src/SearchInput.tsx
+++ react-search-component/my-app/src/SearchInput.tsx
@@ -1,13 +1,22 @@
+import { FiSearch } from 'react-icons/fi';
+
 type Props = {
   onSearchInput: Function;
 };
 
 export default function SearchInput({ onSearchInput }: Props) {
   return (
-    <input
-      type="search"
-      className="search-input"
-      placeholder="Search"
-      onChange={(e) => onSearchInput(e.currentTarget.value)}></input>
+    <div
+      style={{ position: 'relative', display: 'flex', alignItems: 'center' }}>
+      <input
+        style={{ paddingLeft: '1.25rem' }}
+        type="search"
+        className="search-input"
+        placeholder="Search"
+        onChange={(e) => onSearchInput(e.currentTarget.value)}></input>
+      <span style={{ position: 'absolute', left: '0.25rem', marginTop: '2px' }}>
+        <FiSearch />
+      </span>
+    </div>
   );
 }
