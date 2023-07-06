8303e4c3de55c2da7e29874e3ce8a1d21bd6a789
Kyle Kettler <hello@kylekettler.com>
2023-07-06 15:43:08 +0000

    completed node-module-system

---
+++ node-module-system/calculate.js
@@ -0,0 +1,23 @@
+import add from './add.js';
+import subtract from './subtract.js';
+import multiply from './mutliply.js';
+import divide from './divide.js';
+
+const calculation = process.argv[3];
+const a = Number(process.argv[2]);
+const b = Number(process.argv[4]);
+
+const result =
+  calculation === 'plus'
+    ? add(a, b)
+    : calculation === 'minus'
+    ? subtract(a, b)
+    : calculation === 'over'
+    ? divide(a, b)
+    : calculation === 'times'
+    ? multiply(a, b)
+    : undefined;
+
+result === undefined
+  ? console.log('invalid operation')
+  : console.log('result:', result);
