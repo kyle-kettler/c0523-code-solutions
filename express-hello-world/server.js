8331da281be8a158748544c134cfc3d27540098d
Kyle Kettler <hello@kylekettler.com>
2023-07-10 19:14:03 +0000

    moved listen() method to bottom of file

--- express-hello-world/server.js
+++ express-hello-world/server.js
@@ -2,11 +2,11 @@ import express from 'express';
 
 const app = express();
 
-app.listen(8080, () => {
-  console.log('Express server listening on port 8080');
-});
-
 app.use((req, res) => {
   console.log('request type', req.method);
   res.send('<p>Hello, world!<p>');
+});
+
+app.listen(8080, () => {
+  console.log('Express server listening on port 8080');
 });
