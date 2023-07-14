00ec58a07a74706b2ca1b576237dedbc4a8a92f4
Kyle Kettler <hello@kylekettler.com>
2023-07-14 22:18:26 +0000

    completed sql-aggregates

---
+++ sql-aggregates/starring-in.sql
@@ -0,0 +1,9 @@
+select "genres"."name" as "genre",
+       count("films") as "totalFilms"
+  from "actors"
+  join "castMembers" using ("actorId")
+  join "films" using ("filmId")
+  join "filmGenre" using ("filmId")
+  join "genres" using ("genreId")
+  where "firstName" = 'Lisa' and "lastName" = 'Monroe'
+  group by "genre"
